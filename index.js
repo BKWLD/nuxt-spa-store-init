a1061a62fee339136b078dd2e074ab02a0eeae98
Robert Reinhard <robert.reinhard@bukwild.com>
2018-04-23 14:33:43 -0700

    Only run when `spa` mode is enabled

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * Register the no-ssr plugin that hydrates vuex
  */
 const path = require('path')
-module.exports = function (module) {
-	this.addPlugin({
-		src: path.resolve(__dirname, 'plugin.js'),
-		ssr: false,
-	});
+module.exports = function () {
+	if (this.options.mode == 'spa') {
+		this.addPlugin({
+			src: path.resolve(__dirname, 'plugin.js'),
+			ssr: false,
+		});
+	}
 }
