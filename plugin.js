004627f9027a69f2fc174b2c3e832b6b1a48e4b2
Robert Reinhard <robert.reinhard@bukwild.com>
2020-09-10 15:59:27 -0700

    Don’t call nuxtServerInit client side when SSG

--- plugin.js
+++ plugin.js
@@ -2,5 +2,6 @@
  * Return Promise from the nuxtServerInit Store
  */
 module.exports = function(ctx) {
+	if (process.static) return // Don't refetch, will be baked in
 	return ctx.store.dispatch('nuxtServerInit', ctx);
 }
