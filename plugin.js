c1144f2a486c289eed882b5d64b546ace282e4e2
Robert Reinhard <robert.reinhard@bukwild.com>
2020-09-11 08:16:14 -0700

    Revert "Don’t call nuxtServerInit client side when SSG"
    This reverts commit 004627f9027a69f2fc174b2c3e832b6b1a48e4b2.
    
    This broke spa mode and wasn’t necessary (I had misinterpretted a specific project issue).

--- plugin.js
+++ plugin.js
@@ -2,6 +2,5 @@
  * Return Promise from the nuxtServerInit Store
  */
 module.exports = function(ctx) {
-	if (process.static) return // Don't refetch, will be baked in
 	return ctx.store.dispatch('nuxtServerInit', ctx);
 }
